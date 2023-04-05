ad234baa23f117dff7e43d626b823a674938a036
Trainer-Project <mohammedalamnyc@gmail.com>
2023-04-05 18:02:48 -0400

    Add java condition class

---
+++ src/test/java/com/usa/smarttech/VerifyTheValues.java
@@ -0,0 +1,29 @@
+package com.usa.smarttech;
+
+public class VerifyTheValues {
+
+	// How do you verify the expected value
+	// By java if else condition
+	public static void main(String[] args) {
+		// Fahim want sale more than $195
+		// Fahim want buy less than $195
+		int teslaPrice = 195;
+
+		if (teslaPrice > 195) {
+			System.out.println("Fahim sale his stocks");
+		}
+
+		else if (teslaPrice < 195) {
+			System.out.println("Fahim buy the stocks");
+		}
+		
+		else if (teslaPrice == 195) {
+			System.out.println("Fahim want to sale his stock by buying price");
+		}
+
+		else {
+			System.out.println("Didn't buy or sale any stocks");
+
+		}
+	}
+}
