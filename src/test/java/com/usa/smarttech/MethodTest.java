4ee69a63f06ef41d6b55518248c306f5070358f2
Trainer-Project <mohammedalamnyc@gmail.com>
2023-04-06 23:39:38 -0400

    first commit

--- src/test/java/com/usa/smarttech/MethodTest.java
+++ src/test/java/com/usa/smarttech/MethodTest.java
@@ -6,28 +6,20 @@ public class MethodTest {
 	public static void getEmp() {
 		System.out.println(Variable_Test.course);
 	}
-	
 	public int getSalary() {
 		int salary = 1500;
-    return salary;
-		
+    return salary;	
 	}
-	
 	public void getName(String name) {
 		System.out.println(name);
-	}
-	
+	}	
 	public void getNames(String name) {
 		System.out.println(name);
 	}
-	
 	public static void main(String[] args) {
 		MethodTest.getEmp();
 		MethodTest obj = new MethodTest();
 		System.out.println("Alam salary is a : "+obj.getSalary());
 		obj.getName("QA Liton vai");
 	}
-	
-	
-	
 }
