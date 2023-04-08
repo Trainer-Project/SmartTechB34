3b48e503a06777c696c07ceb8c4c43b4b4b39d08
Trainer-Project <mohammedalamnyc@gmail.com>
2023-04-08 00:56:32 -0400

    Add new condition

--- src/test/java/com/usa/smarttech/VerifyTheValues.java
+++ src/test/java/com/usa/smarttech/VerifyTheValues.java
@@ -8,16 +8,19 @@ public class VerifyTheValues {
 		// Fahim want to sale more than $195
 		// Fahim want to buy less than $195
 		int teslaPrice = 195;
-
 		if (teslaPrice > 195) {
 			System.out.println("Fahim sale his stocks");
 		} else if (teslaPrice < 195) {
 			System.out.println("Fahim buy the stocks");
 		} else if (teslaPrice == 195) {
 			System.out.println("Fahim want to sale his stock by buying price");
+
+		} else if (teslaPrice == 195) {
+			System.out.println("Fahim want to sale his stock by buying price");
 		} else {
 			System.out.println("Didn't buy or sale any stocks");
 
 		}
+
 	}
 }
